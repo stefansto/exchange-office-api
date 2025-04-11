f02813c293b89df081d5a46fcefb04ca88424da1
stefansto <stefannuestojic@gmail.com>
2025-04-11 03:44:23 +0200

    Updated server.js to use controllers and package.json tweak

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const crypto = require('crypto');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 
-const uri = '';
+const login = require('./controllers/login');
+const transaction = require('./controllers/transaction');
+const main = require('./controllers/main');
+
+const uri = 'mongodb://localhost:27017';
 const client = new MongoClient(uri);
+const database = client.db('exchange');
 
 const port = 3000;
 
@@ -15,18 +20,20 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    console.log("GET request sent for / with body: ", req.body);
-    res.json("Success");
-})
+    console.log("GET request sent for /");
+    main.handleMain(res, database);
+});
 
 app.post('/login', (req, res) => {
     console.log("POST request sent for /login with body: ", req.body);
-})
+    login.handleLogin(req, res, database);
+});
 
-app.post('/transaction', (req, res) => {
-    console.log("POST request sent for /transaction with body: ", req.body);
-})
+app.put('/transaction', (req, res) => {
+    console.log("PUT request sent for /transaction with body: ", req.body);
+    transaction.handleTransaction(req, res, database);
+});
 
 app.listen(port, ()=>{
     console.log('App is running on port:', port);
-})
+});
