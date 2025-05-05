e8474bd7107e22a57ed15245b880e57ce4539abe
stefansto <stefannuestojic@gmail.com>
2025-05-05 22:38:46 +0200

    Added checkActiveStatus middleware

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const { handleActivateUser } = require('./controllers/admin/activateUser');
 
 const { adminAuthorization } = require('./middleware/adminAuthorization');
 const { cookieJwtAuth } = require('./middleware/cookieJwtAuth');
+const { checkActiveStatus } = require('./middleware/checkActiveStatus');
 
 const { database } = require('./database/connection');
 
@@ -26,17 +27,17 @@ app.use(cors({origin: true, credentials: true}));
 
 app.post('/login', (req, res) => handleLogin(req, res, database));
 
-app.get('/currencies', cookieJwtAuth, (_req, res) => handleGetCurrencies(res, database));
-app.get('/transactions', cookieJwtAuth, (_req, res) => handleGetTransaction(res, database));
+app.get('/currencies', cookieJwtAuth, checkActiveStatus, (_req, res) => handleGetCurrencies(res, database));
+app.get('/transactions', cookieJwtAuth, checkActiveStatus, (_req, res) => handleGetTransaction(res, database));
 
-app.post('/transactions/filtered', cookieJwtAuth, (req, res) => handleFilterTransactions(req, res, database));
+app.post('/transactions/filtered', cookieJwtAuth, checkActiveStatus, (req, res) => handleFilterTransactions(req, res, database));
 
-app.put('/transactions/new', cookieJwtAuth, (req, res) => handlePutTransaction(req, res, database));
+app.put('/transactions/new', cookieJwtAuth, checkActiveStatus, (req, res) => handlePutTransaction(req, res, database));
 
-app.post('/admin/adduser', cookieJwtAuth, adminAuthorization, (req, res) => handleAddUser(req, res, database));
-app.post('/admin/fetchusers', cookieJwtAuth, adminAuthorization, (req, res) => handleFetchUsers(req, res, database));
-app.post('/admin/changeuser', cookieJwtAuth, adminAuthorization, (req, res) => handleChangeUser(req, res, database));
-app.post('/admin/activateuser', cookieJwtAuth, adminAuthorization, (req, res) => handleActivateUser(req, res, database));
+app.post('/admin/adduser', cookieJwtAuth, checkActiveStatus, adminAuthorization, (req, res) => handleAddUser(req, res, database));
+app.post('/admin/fetchusers', cookieJwtAuth, checkActiveStatus, adminAuthorization, (req, res) => handleFetchUsers(req, res, database));
+app.post('/admin/changeuser', cookieJwtAuth, checkActiveStatus, adminAuthorization, (req, res) => handleChangeUser(req, res, database));
+app.post('/admin/activateuser', cookieJwtAuth, checkActiveStatus, adminAuthorization, (req, res) => handleActivateUser(req, res, database));
 
 app.listen(parseInt(process.env.API_PORT), () => {
     console.log('App is running on port:', parseInt(process.env.API_PORT));
